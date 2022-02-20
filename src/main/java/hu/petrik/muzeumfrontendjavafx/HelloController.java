33ec7e4ec8d99f95a963da02fa833af45c0255d5
Dia <balaska.klaudiaa@gmail.com>
2022-02-20 20:04:08 +0100

    Deklarálás

--- src/main/java/hu/petrik/muzeumfrontendjavafx/HelloController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/HelloController.java
@@ -1,14 +1,53 @@
 package hu.petrik.muzeumfrontendjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn col_SzCim;
+    @FXML
+    private TableColumn col_FMagas;
+    @FXML
+    private TableColumn col_SzEv;
+    @FXML
+    private TableColumn col_FId;
+    @FXML
+    private TableColumn col_SzId;
+    @FXML
+    private AnchorPane HelloController;
+    @FXML
+    private TableColumn col_FAr;
+    @FXML
+    private TableColumn col_FSzemely;
+    @FXML
+    private TableColumn col_SzKiallit;
+
+    @FXML
+    public void btn_SzHozzaad(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btn_SzTorol(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btn_FHozzaad(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btn_FModosit(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btn_FTorol(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btn_SzModosit(ActionEvent actionEvent) {
     }
 }

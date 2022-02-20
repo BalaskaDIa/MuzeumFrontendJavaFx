2b5c25a2f4a4b4422572f9362faadaebf893783d
Dia <balaska.klaudiaa@gmail.com>
2022-02-20 20:51:27 +0100

    Javítás, inicializálás

--- src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/MainController.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/controllers/MainController.java
@@ -1,31 +1,47 @@
 package hu.petrik.muzeumfrontendjavafx.controllers;
 
+import hu.petrik.muzeumfrontendjavafx.Festmeny;
+import hu.petrik.muzeumfrontendjavafx.Szobor;
+import hu.petrik.muzeumfrontendjavafx.api.Api;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class MainController {
 
     @FXML
-    private TableColumn col_SzCim;
+    private TableColumn <Festmeny, Integer> col_FId;
     @FXML
-    private TableColumn col_FMagas;
+    private TableColumn <Szobor, Integer> col_SzId;
     @FXML
-    private TableColumn col_SzEv;
+    private TableView <Szobor> table_Szobor;
     @FXML
-    private TableColumn col_FId;
+    private TableView <Festmeny> table_Festmeny;
     @FXML
-    private TableColumn col_SzId;
+    private TableColumn <Szobor, Integer> col_SzAr;
     @FXML
-    private AnchorPane HelloController;
+    private TableColumn <Szobor, String> col_SzSzemely;
     @FXML
-    private TableColumn col_FAr;
+    private TableColumn <Szobor, Integer> col_SzMagas;
     @FXML
-    private TableColumn col_FSzemely;
+    private TableColumn <Festmeny, Integer> col_FEv;
     @FXML
-    private TableColumn col_SzKiallit;
+    private TableColumn <Festmeny, Boolean> col_FKiallit;
+    @FXML
+    private TableColumn <Festmeny, String> col_FCim;
+
+    public void initialize() {
+        col_FId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col_FCim.setCellValueFactory(new PropertyValueFactory<>("title"));
+        col_FEv.setCellValueFactory(new PropertyValueFactory<>("year"));
+        col_FKiallit.setCellValueFactory(new PropertyValueFactory<>("on_display"));
+    }
 
     @FXML
     public void btn_SzHozzaad(ActionEvent actionEvent) {
